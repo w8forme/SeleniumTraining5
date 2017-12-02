11ae5a6062ddd5e49a20cdd48e8a25a31d512fc6
w8forme <velesgp@mail.ru>
2017-12-02 13:31:34 +0200

    Initial commit

---
+++ src/test/java/myprojects/automation/assignment5/pages/MainPage.java
@@ -0,0 +1,40 @@
+package myprojects.automation.assignment5.pages;
+
+import myprojects.automation.assignment5.utils.Properties;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by Pavel Holinko on 02.12.2017.
+ */
+public class MainPage {
+
+    private WebDriver driver;
+
+    @FindBy(css = "a.all-product-link.pull-xs-left.pull-md-right.h4")
+    private WebElement allProductsLink;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public MainPage openMainPage() {
+        driver.get(Properties.getBaseUrl());
+        return this;
+    }
+
+    public MainPage scrollToAllProductsLink() {
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].scrollIntoView(true);", allProductsLink);
+        return this;
+    }
+
+    public AllProductsPage clickAllProductsLink() {
+        allProductsLink.click();
+        return PageFactory.initElements(driver, AllProductsPage.class);
+    }
+}
---
+++ src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
@@ -0,0 +1,59 @@
+package myprojects.automation.assignment5.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+
+public class DriverFactory {
+    /**
+     *
+     * @param browser Driver type to use in tests.
+     * @return New instance of {@link WebDriver} object.
+     */
+    public static WebDriver initDriver(String browser) {
+        switch (browser) {
+            case "firefox":
+                System.setProperty(
+                        "webdriver.gecko.driver",
+                        new File(DriverFactory.class.getResource("/geckodriver.exe").getFile()).getPath());
+                return new FirefoxDriver();
+            case "ie":
+            case "internet explorer":
+                System.setProperty(
+                        "webdriver.ie.driver",
+                        new File(DriverFactory.class.getResource("/IEDriverServer.exe").getFile()).getPath());
+                DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+                capabilities.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);
+                capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+                return new InternetExplorerDriver(capabilities);
+            case "phantomjs":
+                System.setProperty(
+                        "phantomjs.binary.path",
+                        new File(DriverFactory.class.getResource("/phantomjs.exe").getFile()).getPath());
+                return new PhantomJSDriver();
+            case "chrome":
+            default:
+                System.setProperty(
+                        "webdriver.chrome.driver",
+                        new File(DriverFactory.class.getResource("/chromedriver.exe").getFile()).getPath());
+                return new ChromeDriver();
+        }
+    }
+
+    /**
+     *
+     * @param browser Remote driver type to use in tests.
+     * @param gridUrl URL to Grid.
+     * @return New instance of {@link RemoteWebDriver} object.
+     */
+    public static WebDriver initDriver(String browser, String gridUrl) {
+        // TODO prepare capabilities for required browser and return RemoteWebDriver instance
+        throw new UnsupportedOperationException();
+    }
+}
